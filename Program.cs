5298813067ae414c1c8e8fce1bd312200bdf71f7
412565-LucasRius <412565@tecnicatura.frc.utn.edu.ar>
2024-08-20 21:32:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Pila pila = new Pila(5);
+
+pila.Añadir(1);
+pila.Añadir(2);
+pila.Añadir(3);
+pila.Añadir(4);
+pila.Añadir(5);
+
+Console.WriteLine(pila.Primero());
+
+object firstStack = pila.Primero();
+Console.WriteLine(firstStack);
+
+object extractedStack = pila.Extraer();
+Console.WriteLine(extractedStack);
+
+
+Cola cola = new Cola();
+
+cola.Añadir(10);
+cola.Añadir(15);
+cola.Añadir(20);
+cola.Añadir(25);
+cola.Añadir(30);
+
+object first = cola.Primero();
+Console.WriteLine(first);
+
+object extracted = cola.Extraer();
+
+Console.WriteLine(extracted);
+
